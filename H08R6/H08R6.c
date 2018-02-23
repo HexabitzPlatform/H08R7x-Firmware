a584ee8d5877d27b87592747572769c65546cc3e
Asaad <asaad@hexabitz.com>
2018-02-22 20:33:26 -0800

    Added module hardware configuration renamed files

---
+++ H08R6/H08R6.c
@@ -0,0 +1,133 @@
+/*
+    BitzOS (BOS) V0.1.4 - Copyright (C) 2018 Hexabitz
+    All rights reserved
+
+    File Name     : H08R6.c
+    Description   : Source code for module H08R6.
+										IR Time-if-Flight (ToF) Sensor (ST VL53L0CX)
+		
+		Required MCU resources : 
+		
+			>> USARTs 1,2,3,4,5,6 for module ports.
+			>> I2C2 for the ToF sensor.
+			>> GPIOB 2 for ToF interrupt (INT).
+			>> GPIOB 12 for ToF shutdown (XSHUT).
+			
+*/
+	
+/* Includes ------------------------------------------------------------------*/
+#include "BOS.h"
+
+
+/* Define UART variables */
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart3;
+UART_HandleTypeDef huart4;
+UART_HandleTypeDef huart5;
+UART_HandleTypeDef huart6;
+
+
+/* Private variables ---------------------------------------------------------*/
+
+
+/* Private function prototypes -----------------------------------------------*/	
+
+
+/* Create CLI commands --------------------------------------------------------*/
+
+
+/* -----------------------------------------------------------------------
+	|												 Private Functions	 														|
+   ----------------------------------------------------------------------- 
+*/
+
+/* --- H08R6 module initialization. 
+*/
+void Module_Init(void)
+{
+	/* Peripheral clock enable */
+
+	
+	/* Array ports */
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  MX_USART4_UART_Init();
+  MX_USART5_UART_Init();
+  MX_USART6_UART_Init();
+	
+
+}
+
+/*-----------------------------------------------------------*/
+
+/* --- H08R6 message processing task. 
+*/
+Module_Status Module_MessagingTask(uint16_t code, uint8_t port, uint8_t src, uint8_t dst)
+{
+	Module_Status result = H08R6_OK;
+	
+	switch (code)
+	{
+
+
+		
+		default:
+			result = H08R6_ERR_UnknownMessage;
+			break;
+	}			
+
+	return result;	
+}
+
+/*-----------------------------------------------------------*/
+
+/* --- Register this module CLI Commands 
+*/
+void RegisterModuleCLICommands(void)
+{
+
+}
+
+/*-----------------------------------------------------------*/
+
+/* --- Get the port for a given UART. 
+*/
+uint8_t GetPort(UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART4)
+		return P1;
+	else if (huart->Instance == USART2)
+		return P2;
+	else if (huart->Instance == USART6)
+		return P3;
+	else if (huart->Instance == USART3)
+		return P4;
+	else if (huart->Instance == USART1)
+		return P5;
+	else if (huart->Instance == USART5)
+		return P6;
+
+	return 0;
+}
+
+/*-----------------------------------------------------------*/
+
+
+/* -----------------------------------------------------------------------
+	|																APIs	 																 	|
+   ----------------------------------------------------------------------- 
+*/
+
+
+
+/* -----------------------------------------------------------------------
+	|															Commands																 	|
+   ----------------------------------------------------------------------- 
+*/
+
+
+
+
+/************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
