56e067f9096289efb1a38f17a0d06e47edd23dd0
Najibkassab <najeebkassab@hotmail.com>
2020-07-05 22:14:05 +0300

    it’s a release candidate 0.2.2

--- User/topology_1.h
+++ User/topology_1.h
@@ -17,21 +17,22 @@
 #include "stm32f0xx_hal.h"
 	 
 
-#define __N	8					// Number of array modules
+#define __N	2				// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
 
+
 // Topology
-static uint16_t array[_N][7] = {
+static uint16_t array[__N][7] = {
 { _H01R0, 0, 0, 0, _mod2|P5, 0, _mod3|P4},									// Module 1
 { _H01R0, 0, 0, _mod4|P6, 0, _mod1|P4, 0},									// Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H08R6	1
+	#define	H0BR4	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
@@ -48,7 +49,6 @@ static uint16_t array[_N][7] = {
 	#define	_P5pol_reversed	1
 	#define	_P6pol_normal	1
 #endif
-
 
 #ifdef __cplusplus
 }
