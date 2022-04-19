ad070c71ec492089b56fa04eb35e0ce1982f2cdb
HALA-MUMMO\Hala MUMMO <halashem13@gmail.com>
2022-04-19 12:40:53 +0300

    Added: temp_length [ NumOfPorts ] temp_index [ NumOfPorts ]

--- H08R6/H08R6_it.c
+++ H08R6/H08R6_it.c
@@ -12,6 +12,9 @@
 #include "BOS.h"
 
 
+uint8_t temp_length[NumOfPorts] = {0};
+uint8_t temp_index[NumOfPorts] = {0};
+
 /* External variables --------------------------------------------------------*/
 extern uint8_t UARTRxBuf[NumOfPorts][MSG_RX_BUF_SIZE];
 //extern uint8_t UARTTxBuf[3][MSG_TX_BUF_SIZE];
