72ab1c0991664ab8632540eb40a642d13fc56b9a
ADEL\adool <adoolfaki@gmail.com>
2024-09-18 13:53:07 +0300

    Move TOF Driver to Thirdparty

--- H08R7/H08R7.h
+++ H08R7/H08R7.h
@@ -103,6 +103,20 @@
 #define _TOF_XSHUT_PIN                GPIO_PIN_5
 #define _TOF_XSHUT_GPIO_CLK()         __GPIOB_CLK_ENABLE();
 #define NUM_MODULE_PARAMS		1
+
+#define HANDLER_ToF_I2C          hi2c2
+
+#define ToF_SENSOR_I2C_ADDRESS   0x52
+#define _WAITFORINT() 	         __WFI()
+
+#define TOF_PWR_Pin            GPIO_PIN_4
+#define TOF_PWR_GPIO_Port      GPIOE
+#define TOF_INT_Pin            GPIO_PIN_1
+#define TOF_INT_GPIO_Port      GPIOB
+#define TOF_INT_EXTI_IRQn      EXTI1_IRQn
+#define TOF_XSHUT_Pin          GPIO_PIN_5
+#define TOF_XSHUT_GPIO_Port    GPIOA
+
 /* VL53L1X definition */
 #define MIN_MEMS_PERIOD_MS				100
 #define MAX_MEMS_TIMEOUT_MS				0xFFFFFFFF
