0db4a8bce8572e08e114b55d2266aa2ee36f1f3c
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2025-01-20 13:53:09 +0300

    Replace old EEPROM Emulated with new library from ST

--- User/main.c
+++ User/main.c
@@ -7,13 +7,23 @@
  */
 /* Includes ------------------------------------------------------------------*/
 #include "BOS.h"
-
 /* Private variables ---------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
 
 /* Main function ------------------------------------------------------------*/
-
+	uint16_t DataR[2500] = {0};
+	uint32_t count = 0;
+	uint32_t pagestatus119;
+	uint32_t pagestatus120;
+	uint32_t pagestatus121;
+	uint32_t pagestatus122;
+	uint32_t pagestatus123;
+	uint32_t pagestatus124;
+	uint32_t pagestatus125;
+	uint32_t pagestatus126;
+	uint8_t flag1 = 0;
+	BOS_Status statuss;
 int main(void) {
 
 	Module_Init();		//Initialize Module &  BitzOS
@@ -28,50 +38,39 @@ int main(void) {
 /* User Task */
 void UserTask(void *argument) {
 
-
-//	AddPortButton(MOMENTARY_NO, 2);   //Define a button connected to port P1
-//	SetButtonEvents(2, 1, 0, 3, 0, 0, 0, 0, 0,1);    // Activate a click event and a pressed_for_x event for 3 seconds
-
-
-	// put your code here, to run repeatedly.
 	while (1) {
 
-//		taskYIELD();
+	if(flag1 == 1)
+	{
+		for(int i=1; i<1008; i++)
+		{
+			statuss = EE_WriteVariable(i,i);
+			pagestatus119 = GetPageState(0x807B800);
+			pagestatus120 = GetPageState(0x807C000);
+			pagestatus121 = GetPageState(0x807C800);
+			pagestatus122 = GetPageState(0x807D000);
+			pagestatus123 = GetPageState(0x807D800);
+			pagestatus124 = GetPageState(0x807E000);
+			pagestatus125 = GetPageState(0x807E800);
+			pagestatus126 = GetPageState(0x807F000);
+			count++;
+		}
+	}
+		for(int i=1; i<1008; i++)
+		{
+			EE_ReadVariable(i,&DataR[i]);
+			pagestatus119 = GetPageState(0x807B800);
+			pagestatus120 = GetPageState(0x807C000);
+			pagestatus121 = GetPageState(0x807C800);
+			pagestatus122 = GetPageState(0x807D000);
+			pagestatus123 = GetPageState(0x807D800);
+			pagestatus124 = GetPageState(0x807E000);
+			pagestatus125 = GetPageState(0x807E800);
+			pagestatus126 = GetPageState(0x807F000);
+
+		}
 
 	}
 }
-
-//void buttonClickedCallback(uint8_t port){
-//
-//	SendMessageToModule(1,CODE_PING, 0);
-//	Delay_ms(100);
-//
-//	SendMessageToModule(2,CODE_PING, 0);
-//	Delay_ms(100);
-//
-//	SendMessageToModule(1,CODE_PING, 0);
-//	Delay_ms(100);
-//
-//	messageParams[0] = 50;
-//	SendMessageToModule(1,CODE_H01R0_ON, 1);
-//	Delay_ms(500);
-//
-//	SendMessageToModule(2,CODE_PING, 0);
-//	Delay_ms(500);
-//
-//	SendMessageToModule(1,CODE_H01R0_OFF, 0);
-//	Delay_ms(100);
-//
-//	SendMessageToModule(2,CODE_PING, 0);
-//	Delay_ms(100);
-//
-//	messageParams[0] = 50;
-//	SendMessageToModule(1,CODE_H01R0_ON, 1);
-//	Delay_ms(500);
-//
-//	SendMessageToModule(1,CODE_H01R0_OFF, 0);
-//	Delay_ms(100);
-//
-//}
 
 /*-----------------------------------------------------------*/
