a9519790b95cdb417af2127770d0acba7427af7c
Mahmood_Reda <mr4141705@gmail.com>
2025-03-26 16:42:41 +0300

    Added GetModuleParameter() to sample module parameters based on index.

--- H08R7/H08R7.c
+++ H08R7/H08R7.c
@@ -60,11 +60,16 @@ typedef void (*SampleMemsToPort)(uint8_t, uint8_t);
 typedef void (*SampleMemsToString)(char*, size_t);
 typedef void (*SampleMemsToBuffer)(uint16_t *buffer);
 /* Module exported parameters ------------------------------------------------*/
-float H08R7_range = 0.0f;
+/* Global variable for ToF sensor data */
+uint16_t H08R7_distance = 0;
+
 float temp __attribute__((section(".mySection")));
 float sample __attribute__((section(".mySection")));
-module_param_t modParam[NUM_MODULE_PARAMS] = { { .paramPtr = &H08R7_range,
-		.paramFormat = FMT_FLOAT, .paramName = "range" } };
+/* Exported Typedef */
+module_param_t modParam[NUM_MODULE_PARAMS] = {
+    { .paramPtr = &H08R7_distance, .paramFormat = FMT_UINT16, .paramName = "distance" }
+};
+
 
 /* Private variables ---------------------------------------------------------*/
 //uint8_t port1, module1;
@@ -526,6 +531,35 @@ uint8_t GetPort(UART_HandleTypeDef *huart) {
 
 	return 0;
 }
+/***************************************************************************/
+/*
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex, float *value) {
+    Module_Status status = H08R7_OK;
+
+    switch (paramIndex) {
+        /* Sample ToF Distance sensor */
+        case 1:
+        {
+            uint16_t temp = 0;
+            status = Sample_ToF(&temp);
+            if (status == H08R7_OK) *value = (float)temp;
+            break;
+        }
+
+        /* Invalid parameter index */
+        default:
+            status = H08R7_ERR_WrongParams;
+            break;
+    }
+
+    return status;
+}
+
 
 /*-----------------------------------------------------------*/
 
