74c8934bd09ab80822f8edac49d188c2bae2ddfc
Muhammad Alhaddad <ubuntu.msa@gmail.com>
2025-04-18 11:08:29 +0300

    Remove doublicated decliration of GetPortGPIO function

--- H08R7/H08R7_inputs.h
+++ H08R7/H08R7_inputs.h
@@ -2,7 +2,7 @@
  BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
- File Name     : BOS_inputs.h
+ File Name     : H08R7_inputs.h
  Description   : header file for Bitz digital and analog inputs.
  
 */
@@ -16,7 +16,6 @@
 /***************************************************************************/
 /* Exported Functions Prototypes *******************************************/
 /***************************************************************************/
-extern BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t *RX_Port,uint16_t *RX_Pin);
 extern void ReadTempAndVref(float *temp,float *Vref);
 extern void ReadADCChannel(uint8_t Port,char *side,float *ADC_Value);
 extern void ADCSelectChannel(uint8_t ADC_port,char *side);
