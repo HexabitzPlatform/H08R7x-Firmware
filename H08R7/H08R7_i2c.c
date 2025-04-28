42e8d42088c263715c661ba83332b10b45accedd
Muhammad Alhaddad <ubuntu.msa@gmail.com>
2025-04-28 21:01:13 +0300

    Move macros from H08R7.h to i2c.h and refactory i2c.c file

--- H08R7/H08R7_i2c.c
+++ H08R7/H08R7_i2c.c
@@ -36,22 +36,22 @@ void MX_I2C_Init(void)
 void MX_I2C2_Init(void) {
 
 	/* Initialize I2C2 peripheral */
-	hi2c2.Instance = I2C2;
-	hi2c2.Init.Timing = 0x10B17DB5; // Normal mode (100 kHz)
-	hi2c2.Init.OwnAddress1 = 0; // No specific address required for master mode
-	hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT; // 7-bit addressing mode
-	hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE; // Disable dual address mode
-	hi2c2.Init.OwnAddress2 = 0; // Not used, set to 0
-	hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK; // No mask for second address
-	hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE; // Disable general call
-	hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE; // Disable clock stretching
-	HAL_I2C_Init(&hi2c2);
+	HANDLER_I2C.Instance = Instance_I2C;
+	HANDLER_I2C.Init.Timing = 0x10B17DB5; // Normal mode (100 kHz)
+	HANDLER_I2C.Init.OwnAddress1 = 0; // No specific address required for master mode
+	HANDLER_I2C.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT; // 7-bit addressing mode
+	HANDLER_I2C.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE; // Disable dual address mode
+	HANDLER_I2C.Init.OwnAddress2 = 0; // Not used, set to 0
+	HANDLER_I2C.Init.OwnAddress2Masks = I2C_OA2_NOMASK; // No mask for second address
+	HANDLER_I2C.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE; // Disable general call
+	HANDLER_I2C.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE; // Disable clock stretching
+	HAL_I2C_Init(&HANDLER_I2C);
 
 	/** Configure Analogue filter */
-	HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE); // Enable analog filter
+	HAL_I2CEx_ConfigAnalogFilter(&HANDLER_I2C, I2C_ANALOGFILTER_ENABLE); // Enable analog filter
 
 	/** Configure Digital filter */
-	HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0); // Digital filter set to 0 (disabled)
+	HAL_I2CEx_ConfigDigitalFilter(&HANDLER_I2C, 0); // Digital filter set to 0 (disabled)
 }
 
 /***************************************************************************/
@@ -61,7 +61,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
 
   GPIO_InitTypeDef GPIO_InitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
-  if(i2cHandle->Instance==I2C2)
+  if(i2cHandle->Instance==Instance_I2C)
   {
   /* USER CODE BEGIN I2C2_MspInit 0 */
 
@@ -96,7 +96,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
 void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
 {
 
-  if(i2cHandle->Instance==I2C2)
+  if(i2cHandle->Instance==Instance_I2C)
   {
   /* USER CODE BEGIN I2C2_MspDeInit 0 */
 
