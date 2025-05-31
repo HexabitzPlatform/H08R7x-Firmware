f0cacac276bb3f3909e218c3307ac700e01506d1
Mahmood_Reda <mr4141705@gmail.com>
2025-05-31 14:20:04 +0300

    clear topology & main files.

--- User/main.c
+++ User/main.c
@@ -1,54 +1,38 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2025 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : main.c
  Description   : Main program body.
  */
-/* Includes ------------------------------------------------------------------*/
+
+/* Includes ****************************************************************/
 #include "BOS.h"
 
-/* Private variables ---------------------------------------------------------*/
-float readValue;
-/* Private function prototypes -----------------------------------------------*/
+/* Private variables *******************************************************/
 
-/* Main function ------------------------------------------------------------*/
+/* Private Function Prototypes *********************************************/
 
-int main(void) {
+/* Main Function ***********************************************************/
+int main(void){
 
-	Module_Init();		//Initialize Module &  BitzOS
+	/* Initialize Module &  BitzOS */
+	Module_Init();
 
-	//Don't place your code here.
-	for (;;) {
+	/* Don't place your code here */
+	for(;;){
 	}
 }
 
-/*-----------------------------------------------------------*/
-uint16_t d ;
-uint32_t apb1ClockFreq, ahbClockFreq, systemClockFreq;
+/***************************************************************************/
 /* User Task */
-void UserTask(void *argument) {
+void UserTask(void *argument){
 
-	// put your code here, to run repeatedly.
-//	StreamToTerminal(2, 10, 1000);
-//	ADCSelectChannel(2,"bottom");
-	while (1) {
+	/* put your code here, to run repeatedly. */
+	while(1){
 
-		ReadADCChannel(2, "bottom", &readValue);
-		HAL_Delay(1000);
-
-		// Get the APB1 peripheral clock frequency (used for peripherals like UART and TIMx in some cases)
-		 apb1ClockFreq = HAL_RCC_GetPCLK1Freq();
-
-		// Get the AHB clock frequency (HCLK), which is used for the CPU, memory, and other controllers
-		 ahbClockFreq = HAL_RCC_GetHCLKFreq();
-
-		// Get the system clock frequency (SYSCLK), which serves as the main clock source for the system
-		 systemClockFreq = HAL_RCC_GetSysClockFreq();
-
-
-		SampleTOF(&d);
 	}
 }
 
-/*-----------------------------------------------------------*/
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
